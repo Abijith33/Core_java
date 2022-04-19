78aabf3a32d890838aca47cf0fb444cfc0dfec87
Admin <Admin@DESKTOP-V433FQI>
2022-04-19 18:36:39 +0530

    first commit

--- HelloWorld1/src/nandu/claobj.java
+++ HelloWorld1/src/nandu/claobj.java
@@ -1,5 +1,13 @@
 package nandu;
 
 public class claobj {
-
+	int yearofpurchase;
+	String model;
+	double cost;
+	
+	public void displ()
+	{
+		System.out.println("Year of purchase"+2001);
+	}
+	
 }
